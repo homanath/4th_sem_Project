150e6edaeafb2eae1b0f7edf9b0d181c6642866e
UmeshMehta1 <ukmehta1245@gmail.com>
2024-12-16 10:29:25 +0545

    backend error fixed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
-const express= require("express")
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/indexRoute");
 
-app.get("/", (req, res)=>{
-    res.send("Hello, World!")
-})
+const app = express();
+require("dotenv").config();
 
 
-app.listen(3000,()=>{
-    console.log("server is runnning port no. 3000")
-})
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api", routes); // All routes will be prefixed with /api
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,15 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 
-const User = sequelize.define('User', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.STRING, defaultValue: 'client' }, // Default role is client
-});
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false }, // "admin", "lawyer", "client"
+  });
+  return User;
+};
 
-module.exports = User;
+
+
